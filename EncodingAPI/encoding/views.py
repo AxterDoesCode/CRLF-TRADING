c06ded0c95aa05dbf8a104a721c9f4cd982898e0
Deniz Alkan <denizalkan@HachiRoku-5.local>
2025-11-01 19:30:46 +0000

    decode

--- EncodingAPI/encoding/views.py
+++ EncodingAPI/encoding/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .encode import Encode
+from .encode import Encode, Decode
 
 class EncodeView(APIView):
 
     def post(self, request):
         data = request.data
-        encode = Encode()
+        encode = Encode(data)
         binary_encoding = encode.encode_to_binary()
         # print("binary encoding: ", binary_encoding)
         deck_encoding = encode.encode_to_deck(binary_encoding)
         # print("deck encoding: ", deck_encoding)
         return Response({"deck_encoding": deck_encoding})
+    
+class DecodeView(APIView):
+    def post(self, request):
+        data = request.data
+        decode = Decode()
+        binary_decoding = decode.decode_from_deck_to_binary(data)
+        stock = decode.decode_from_binary_to_stock(binary_decoding)
+        return Response({"stock": stock})
