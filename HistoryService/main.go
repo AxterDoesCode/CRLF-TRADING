a9954852b9d03b4c2d7afbff8f6094443c2f385c
Ofsouzap <ofsouzap@gmail.com>
2025-11-02 12:59:29 +0000

    remove changes

--- HistoryService/main.go
+++ HistoryService/main.go
@@ -22,8 +22,9 @@ const ROYALE_API_URL = "https://proxy.royaleapi.dev/v1"
 const ENCODE_API_URL = "http://localhost:8000/encoding/encode"
 const DECODE_API_URL = "http://localhost:8000/encoding/decode"
 const TRADING_API_URL = "http://localhost:3003"
-const PLAYERTAGURI = "%232LGQJVR2C"
-const PLAYERTAG = "2LGQJVR2C"
+
+const PLAYERTAGURI = "%232YLCP0R8"
+const PLAYERTAG = "2YLCP0R8"
 
 func main() {
 	client := &http.Client{}
@@ -256,8 +257,6 @@ func makeTrade(c *http.Client, trade TradeAction) error {
 	startOfDay := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	// Compute seconds since start of the day
 	secondsSinceStart := int64(now.Sub(startOfDay).Seconds())
-
-	fmt.Println("T", secondsSinceStart)
 
 	tradeStruct := TradeStruct{
 		PlayerID: PLAYERTAG,
